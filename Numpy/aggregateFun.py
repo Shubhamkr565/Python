e856753af2b289bc0633d3eda3186de65350624d
Shubham <shubhamkr565@gmail.com>
2026-04-29 12:34:12 +0530

    AggregateFun

---
+++ Numpy/aggregateFun.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Aggregate function = summarize data and typically return a single value
+
+array = np.array([[1,2,3,4,5],
+                  [6,7,8,9,10]])
+
+# print(np.sum(array))
+# print(np.mean(array))
+
+# print(np.std(array))
+
+# print(np.var(array))
+
+# print(np.min(array))
+
+# print(np.max(array))
+
+
+# print(np.argmin(array))
+
+# print(np.argmax(array))
+
